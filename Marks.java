9821c9b1875940651c9681a71ffca32b6c5e4096
misbahul30 <126447342+misbahul30@users.noreply.github.com>
2023-03-06 03:51:34 +0800

    Update Marks.java

--- Marks.java
+++ Marks.java
@@ -1,51 +1,51 @@
 public class Marks {
 
-	float marks[];
-	int numdata;
+	float marks[]; // array to store marks
+	int numdata; // number of data points
 	
 	//default constructor
 	public Marks() {		
 	}
 	//class constructor
-	public Marks(int numdata) {
+	public Marks(int numdata) { // constructor to set number of data points
 		this.numdata = numdata;
 	}
 	
 	//getter and setter method
-	public void setMarks(int marks, int index) {
+	public void setMarks(int marks, int index) { // setter method to set marks at a given index
 		this.marks[index] = marks;
 	}
-	public float getMarks(int index) {
+	public float getMarks(int index) { // getter method to get marks at a given index
 		return marks[index];
 	}
 	
 	//just an operations
-	void createData(){
-		float[] marks = new float[numdata];
+	void createData(){ // method to generate data points
+		float[] marks = new float[numdata]; // initialize array
 		for(int i =0; i<numdata; i++) {
-			marks[i]=i;			
+			marks[i]=i; // populate array with data points
 		}
-		this.marks = marks;
+		this.marks = marks; // set array to marks variable
 	}
 	
-	float average(){
+	float average(){ // method to calculate average
 		float sum =0;
         for (int i=0; i<numdata; i++) {
             sum += marks[i];
-            }
-        float avg = sum / numdata;
+        }
+        float avg = sum / numdata; // calculate average
         return avg;
 	}
 	
-	float stanDev(float avg){
+	float stanDev(float avg){ // method to calculate standard deviation
 		 float sum = 0;
 	        float sd;
 
 	        for(int i =0; i< numdata; i++){
-	            sum += Math.pow((marks[i]-avg),2);
+	            sum += Math.pow((marks[i]-avg),2); // calculate sum of squares
 	        };
 
-	        sd = (float) Math.sqrt(sum/(numdata-1));
+	        sd = (float) Math.sqrt(sum/(numdata-1)); // calculate standard deviation
 	        return sd;
 	}
 }
