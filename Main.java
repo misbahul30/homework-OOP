ac78f26b176d1b5c958b21c3bd5344fd87958aed
misbahul30 <126447342+misbahul30@users.noreply.github.com>
2023-03-06 03:22:02 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,42 @@
 import java.util.Scanner;
 
 public class Main { 
-	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("Enter number of data: ");
-		int numdata = scanner.nextInt();
-		
-		Marks marks = new Marks(numdata);
-		marks.createData();
-		PrincipalName principalName = new PrincipalName("Misbahul", "Munir");
-		Address address = new Address("No 123, Jalan Paloh Hinai", "12345", " Kuching", "Sarawak");
-		School school = new School("Sekolah Siput balik",address,principalName,marks);
-	
-		school.setNumberParticipants();
-		System.out.println(school.toString());
-		System.out.printf("Average Score     : %.2f",marks.average());
-		System.out.printf("\nStandard Deviation: %.2f",marks.stanDev(marks.average()));
-	}
+    public static void main(String[] args) {
+        
+        // Create a Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+        
+        // Ask the user to input the number of data
+        System.out.print("Enter number of data: ");
+        
+        // Read the number of data entered by the user
+        int numdata = scanner.nextInt();
+        
+        // Create a Marks object with the specified number of data
+        Marks marks = new Marks(numdata);
+        
+        // Populate the data for the Marks object
+        marks.createData();
+        
+        // Create a PrincipalName object with the name of the school principal
+        PrincipalName principalName = new PrincipalName("Misbahul", "Munir");
+        
+        // Create an Address object with the address details of the school
+        Address address = new Address("No 123, Jalan Paloh Hinai", "12345", " Kuching", "Sarawak");
+        
+        // Create a School object with the school name, address, principal name, and marks
+        School school = new School("Sekolah Siput balik", address, principalName, marks);
+    
+        // Set the number of participants for the School object
+        school.setNumberParticipants();
+        
+        // Print the details of the School object
+        System.out.println(school.toString());
+        
+        // Calculate and print the average score for the Marks object
+        System.out.printf("Average Score     : %.2f", marks.average());
+        
+        // Calculate and print the standard deviation for the Marks object
+        System.out.printf("\nStandard Deviation: %.2f", marks.stanDev(marks.average()));
+    }
 }
