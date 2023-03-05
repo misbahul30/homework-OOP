523d2843aab56dbd2df42d1cb3a96da2b9640ee8
misbahul30 <126447342+misbahul30@users.noreply.github.com>
2023-03-06 03:28:00 +0800

    Update Address.java

--- Address.java
+++ Address.java
@@ -1,51 +1,52 @@
-
 public class Address {
 
-	private String street;
-	private String postcode;
-	private String district;
-	private String state;
-	
-	//default constructor
-	public Address(){
-	}
-	//class constructor
-	public Address(String street, String postcode, String district, String state){
-		this.street = street;
-		this.postcode = postcode;
-		this.district = district;
-		this.state = state;
-	}
-	
-	//getter and setter method
-	public String getStreet() {
-		return street;
-	}
-	public void setStreet(String street) {
-		this.street = street;
-	}
-	public String getPostcode() {
-		return postcode;
-	}
-	public void setPostcode(String postcode) {
-		this.postcode = postcode;
-	}
-	public String getDistrict() {
-		return district;
-	}
-	public void setDistrict(String district) {
-		this.district = district;
-	}
-	public String getState() {
-		return state;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	
-	//toString method
-	public String toString() {
-		return street + " " + postcode + " " + district + " " + state;
-	}
-	
+    // Declare instance variables for the address fields
+    private String street;
+    private String postcode;
+    private String district;
+    private String state;
+    
+    // Default constructor
+    public Address(){
+    }
+    
+    // Class constructor
+    public Address(String street, String postcode, String district, String state){
+        // Initialize the instance variables with the values passed to the constructor
+        this.street = street;
+        this.postcode = postcode;
+        this.district = district;
+        this.state = state;
+    }
+    
+    // Getter and setter methods for each instance variable
+    public String getStreet() {
+        return street;
+    }
+    public void setStreet(String street) {
+        this.street = street;
+    }
+    public String getPostcode() {
+        return postcode;
+    }
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+    public String getDistrict() {
+        return district;
+    }
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    
+    // toString method that returns a string representation of the address
+    public String toString() {
+        return street + " " + postcode + " " + district + " " + state;
+    }
 }
